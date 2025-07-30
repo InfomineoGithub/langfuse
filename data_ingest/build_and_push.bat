ea75559adebe8fbb2b1db4daca722ce82dd3d269
hazemHany09 <hazem.hany@infomineo.com>
2025-07-30 18:54:24 +0300

    update enc

--- data_ingest/build_and_push.bat
+++ data_ingest/build_and_push.bat
@@ -1,11 +1,11 @@
 
 
 echo --- Building Docker Image ---
-docker build -t europe-west1-docker.pkg.dev/brain-staging-450115/clickhouse-analytics/clickhouse-analytics .
+docker build -t europe-west1-docker.pkg.dev/brain-prod-450115/clickhouse-analytics/clickhouse-analytics .
 
 echo.
 echo --- Pushing Docker Image to GCR ---
-docker push europe-west1-docker.pkg.dev/brain-staging-450115/clickhouse-analytics/clickhouse-analytics
+docker push europe-west1-docker.pkg.dev/brain-prod-450115/clickhouse-analytics/clickhouse-analytics
 
 echo.
 echo --- Script finished ---
